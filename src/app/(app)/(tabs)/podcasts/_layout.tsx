06855713236f25393894dffdfc3580ff0f8385af
Keith Kurak <keith.kurak@gmail.com>
2024-06-23 10:27:35 -0400

    fix back after deep link to podcasts

--- src/app/(app)/(tabs)/podcasts/_layout.tsx
+++ src/app/(app)/(tabs)/podcasts/_layout.tsx
@@ -1,6 +1,11 @@
 import React from "react"
 import { Stack } from "expo-router"
 
+export const unstable_settings = {
+  // Ensure any route can link back to `/`
+  initialRouteName: 'index',
+};
+
 export default function StackLayout() {
   return <Stack screenOptions={{ headerShown: false }} />
 }
