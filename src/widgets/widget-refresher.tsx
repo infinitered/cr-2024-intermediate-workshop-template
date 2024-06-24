45f0e8b3ff6ed7569b395036a1c1271ea93b0699
Keith Kurak <keith.kurak@gmail.com>
2024-06-24 00:21:59 -0400

    ugly but semi functional ios widget

--- src/widgets/widget-refresher.tsx
+++ src/widgets/widget-refresher.tsx
@@ -1,14 +1,21 @@
 import React from "react"
+import { Platform } from "react-native"
 import { requestWidgetUpdate } from "react-native-android-widget"
 import { Episode } from "src/models/Episode"
 import { HelloWidget } from "./android/HelloWidget"
+import IosWidgetRefresh from "../../modules/ios-widget-refresh";
 
 export const updateEpisodesWidget = (episodes: Episode[]) => {
-  requestWidgetUpdate({
-    widgetName: "Hello",
-    renderWidget: (props) => <HelloWidget episodes={episodes} widgetInfo={props} />,
-    widgetNotFound: () => {
-      // Called if no widget is present on the home screen
-    },
-  })
+  if (Platform.OS === "android") {
+    requestWidgetUpdate({
+      widgetName: "Hello",
+      renderWidget: (props) => <HelloWidget episodes={episodes} widgetInfo={props} />,
+      widgetNotFound: () => {
+        // Called if no widget is present on the home screen
+      },
+    })
+  }
+  if (Platform.OS === "ios") {
+    IosWidgetRefresh.set( "episodes", JSON.stringify(episodes.slice()), "group.cr2024im.data")
+  }
 }
--- targets/widgets/widgets.swift
+++ targets/widgets/widgets.swift
@@ -8,35 +8,86 @@
 import WidgetKit
 import SwiftUI
 
+struct EpisodeFromStore: Codable {
+  let guid: String
+  let title: String
+  let thumbnail: String
+}
+
+enum FetcherError: Error {
+    case imageDataCorrupted
+}
+
+extension UIImage {
+  func resized(toWidth width: CGFloat, isOpaque: Bool = true) -> UIImage? {
+    let canvas = CGSize(width: width, height: CGFloat(ceil(width/size.width * size.height)))
+    let format = imageRendererFormat
+    format.opaque = isOpaque
+    return UIGraphicsImageRenderer(size: canvas, format: format).image {
+      _ in draw(in: CGRect(origin: .zero, size: canvas))
+    }
+  }
+}
+
 struct Provider: TimelineProvider {
+  func fetchImage(url: String) async throws -> UIImage {
+      // Download image from URL
+    let (imageData, _) = try await URLSession.shared.data(from: URL(string: url)!)
+      
+      guard let image = UIImage(data: imageData) else {
+        throw FetcherError.imageDataCorrupted
+      }
+      
+      return image
+  }
     func placeholder(in context: Context) -> SimpleEntry {
-        SimpleEntry(date: Date(), emoji: "😀")
+      SimpleEntry(date: Date(), title: "RNR 1: The First One", imageData: nil, episodeCount: 1)
     }
 
     func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
-        let entry = SimpleEntry(date: Date(), emoji: "😀")
+        let entry = SimpleEntry(date: Date(), title: "RNR 1: The First One", imageData: nil, episodeCount: 1)
         completion(entry)
     }
 
     func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
-        var entries: [SimpleEntry] = []
-
-        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
-        let currentDate = Date()
-        for hourOffset in 0 ..< 5 {
-            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
-            let entry = SimpleEntry(date: entryDate, emoji: "😀")
-            entries.append(entry)
+      Task {
+        // Notice the App Group is used here
+        let userDefaults = UserDefaults(suiteName: "group.cr2024im.data")
+        // And the key for the data that we use in the Zustand store.
+        let episodesJsonString = userDefaults?.string(forKey: "episodes") ?? "[]"
+        
+        let decoded: [EpisodeFromStore] = try! JSONDecoder().decode([EpisodeFromStore].self, from: Data(episodesJsonString.utf8))
+        
+        let firstEpisode = decoded.first
+        
+        if (firstEpisode != nil) {
+          guard let image = try? await fetchImage(url: firstEpisode?.thumbnail ?? "") else {
+            return
+          }
+          
+          // pass the data to the widget
+          let entry = SimpleEntry(date: Date(), title: firstEpisode?.title ?? "", imageData: image.resized(toWidth: 100), episodeCount: decoded.count)
+          
+          // Some other stuff to make the widget update...
+          let timeline = Timeline(entries: [entry], policy: .atEnd)
+          completion(timeline)
+        } else {
+          // pass the data to the widget
+          let entry = SimpleEntry(date: Date(), title: "", imageData: nil, episodeCount: 0)
+          
+          // Some other stuff to make the widget update...
+          let timeline = Timeline(entries: [entry], policy: .atEnd)
+          completion(timeline)
         }
-
-        let timeline = Timeline(entries: entries, policy: .atEnd)
-        completion(timeline)
+      }
     }
 }
 
 struct SimpleEntry: TimelineEntry {
     let date: Date
-    let emoji: String
+    let title: String
+    var imageData: UIImage?
+    let episodeCount: Int
 }
 
 struct HelloWidgetEntryView : View {
@@ -44,11 +95,24 @@ struct HelloWidgetEntryView : View {
 
     var body: some View {
         VStack {
-            Text("Time:")
-            Text(entry.date, style: .time)
-
-            Text("Emoji:")
-            Text(entry.emoji)
+          if (entry.episodeCount == 0) {
+            Text("No favorite episodes yet!")
+          } else {
+            HStack {
+              if (entry.imageData !== nil) {
+                Image(uiImage: entry.imageData!)
+                  .resizable()
+                  .scaledToFill()
+                  .frame(width: 100, height:100)
+                  .cornerRadius(10)
+                  .scaledToFill()
+                  .frame(width: 100, height:100)
+                  .cornerRadius(10)
+                
+              }
+              Text(entry.title)
+            }
+          }
         }
     }
 }
@@ -75,6 +139,5 @@ struct HelloWidget: Widget {
 #Preview(as: .systemSmall) {
     HelloWidget()
 } timeline: {
-    SimpleEntry(date: .now, emoji: "😀")
-    SimpleEntry(date: .now, emoji: "🤩")
+  SimpleEntry(date: Date(), title: "RNR 1: The First One", imageData: nil, episodeCount: 1)
 }
