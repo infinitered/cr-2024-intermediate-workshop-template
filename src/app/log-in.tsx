0c127ba0dff661c7feae3fd08ffeb37665d08b0f
Frank Calise <fcalise@gmail.com>
2024-07-08 11:48:41 -0400

    fix: index route

--- src/app/log-in.tsx
+++ src/app/log-in.tsx
@@ -15,7 +15,13 @@ export default observer(function Login(_props) {
   const [isSubmitted, setIsSubmitted] = useState(false)
   const [attemptsCount, setAttemptsCount] = useState(0)
   const {
-    authenticationStore: { authToken, authEmail, setAuthEmail, setAuthToken, validationError },
+    authenticationStore: {
+      isAuthenticated,
+      authEmail,
+      setAuthEmail,
+      setAuthToken,
+      validationError,
+    },
   } = useStores()
 
   useEffect(() => {
@@ -68,7 +74,7 @@ export default observer(function Login(_props) {
     [isAuthPasswordHidden],
   )
 
-  if (authToken) {
+  if (isAuthenticated) {
     return <Redirect href="/(app)/(tabs)/showroom" />
   }
 
