6cfc6db26db3cdffc00b6e3ce3f23331bf603247
Frank Calise <fcalise@gmail.com>
2024-07-12 15:22:11 -0400

    05: hello widget android

---
+++ src/widgets/android/widget-task-handler.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import type { WidgetTaskHandlerProps } from "react-native-android-widget";
+import { HelloWidget } from "./HelloWidget";
+
+export async function widgetTaskHandler(props: WidgetTaskHandlerProps) {
+  switch (props.widgetAction) {
+    case "WIDGET_ADDED":
+    case "WIDGET_UPDATE":
+    case "WIDGET_RESIZED":
+      props.renderWidget(<HelloWidget />);
+      break;
+    default:
+      break;
+  }
+}
