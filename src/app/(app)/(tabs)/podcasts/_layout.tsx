18c3a352ddffc4379aa07d1b9422578aa5ec3f84
Frank Calise <fcalise@gmail.com>
2024-07-12 07:24:54 -0400

    04: implemented quick actions!

--- src/app/(app)/(tabs)/podcasts/_layout.tsx
+++ src/app/(app)/(tabs)/podcasts/_layout.tsx
@@ -4,3 +4,9 @@ import { Stack } from "expo-router"
 export default function StackLayout() {
   return <Stack screenOptions={{ headerShown: false }} />
 }
+
+// eslint-disable-next-line camelcase
+export const unstable_settings = {
+  // Ensure any route can link back to `/`
+  initialRouteName: "index",
+}
--- src/app/(app)/_layout.tsx
+++ src/app/(app)/_layout.tsx
@@ -1,8 +1,11 @@
 import React from "react"
 import { Redirect, Stack } from "expo-router"
 import { observer } from "mobx-react-lite"
+import * as QuickActions from "expo-quick-actions"
+import { useQuickActionRouting, RouterAction } from "expo-quick-actions/router"
 import { useStores } from "src/models"
 import { useThemeProvider } from "src/utils/useAppTheme"
+import { Platform } from "react-native"
 
 export default observer(function Layout() {
   const {
@@ -11,6 +14,27 @@ export default observer(function Layout() {
       profile: { darkMode },
     },
   } = useStores()
+
+  useQuickActionRouting()
+
+  React.useEffect(() => {
+    QuickActions.setItems<RouterAction>([
+      {
+        title: "Update your profile",
+        subtitle: "Keep your deets up-to-date!",
+        icon: Platform.OS === "android" ? "profile_icon" : "contact",
+        id: "0",
+        params: { href: "/(app)/(tabs)/profile" },
+      },
+      {
+        title: "Check out the latest podcast",
+        subtitle: "What's everyone saying on RN Radio?",
+        icon: Platform.OS === "android" ? "podcast_icon" : "play",
+        id: "1",
+        params: { href: "/(app)/(tabs)/podcasts/latest" },
+      },
+    ])
+  }, [])
 
   const { themeScheme, setThemeContextOverride, ThemeProvider } = useThemeProvider(
     darkMode ? "dark" : "light",
