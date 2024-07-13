90e9f95fcd70835b62b053817f7139124381f0c8
Frank Calise <fcalise@gmail.com>
2024-07-12 20:17:27 -0400

    05: ex 01 hello widget ios

---
+++ targets/widgets/expo-target.config.js
@@ -0,0 +1,9 @@
+/** @type {import('@bacons/apple-targets').Config} */
+module.exports = {
+  type: "widget",
+  colors: {
+    $accent: "#F09458",
+    $widgetBackground: "#DB739C",
+  },
+  "deploymentTarget": "17.0"
+};
